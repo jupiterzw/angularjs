de24b58f6808c0d77a95d44fd30f2ffacd220957
jupiterzw <xiarifengdong2007@qq.com>
2016-04-26 16:21:24 +0800

    Added files via upload

---
+++ switchable-grid/script.js
@@ -0,0 +1,58 @@
+// Define a new module. This time we declare a dependency on
+// the ngResource module, so we can work with the Instagram API
+
+var app = angular.module("switchableGrid",[]);
+
+
+// The controller. Notice that I've included our instagram service which we
+// defined below. It will be available inside the function automatically.
+
+function SwitchableGridController($scope){
+
+	// Default layout of the app. Clicking the buttons in the toolbar
+	// changes this value.
+
+	$scope.layout = 'grid';
+
+	$scope.pics = [];
+
+	// Use the instagram service and fetch a list of the popular pics
+	$scope.pics = [
+		{
+			url: 'http://tutorialzine.com/2013/07/50-must-have-plugins-for-extending-twitter-bootstrap/',
+			title: '50 Must-have plugins for extending Twitter Bootstrap',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/07/featured_4-100x100.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com/2013/08/simple-registration-system-php-mysql/',
+			title: 'Making a Super Simple Registration System With PHP and MySQL',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/08/simple_registration_system-100x100.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com/2013/08/slideout-footer-css/',
+			title: 'Create a slide-out footer with this neat z-index trick',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/08/slide-out-footer-100x100.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com/2013/06/digital-clock/',
+			title: 'How to Make a Digital Clock with jQuery and CSS3',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/06/digital_clock-100x100.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com/2013/05/diagonal-fade-gallery/',
+			title: 'Smooth Diagonal Fade Gallery with CSS3 Transitions',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/05/featured-100x100.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com/2013/05/mini-ajax-file-upload-form/',
+			title: 'Mini AJAX File Upload Form',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/05/ajax-file-upload-form-100x100.jpg'
+		},
+		{
+			url: 'http://tutorialzine.com/2013/04/services-chooser-backbone-js/',
+			title: 'Your First Backbone.js App – Service Chooser',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/04/service_chooser_form-100x100.jpg'
+		}
+	];
+
+}
